c625e1f7ce71f15a712f0c5db9974ca55ff13153
[Klara Margrethe Helgemo] <[klara.margrethe.helgemo@nav.no]>
2021-10-05 14:06:10 +0200

    Fjerner overflødig tabell-header

--- src/components/body/table-header/table-header.tsx
+++ src/components/body/table-header/table-header.tsx
@@ -3,73 +3,8 @@ import React from 'react';
 export const TableHeader = () => {
 
 
-	return (
-		<div role="rowgroup">
-			<div role="row" className="table-header">
-				<HeaderField
-					name={HeaderFieldName.BRUKER_ETTERNAVN}
-					text="Etternavn, Fornavn"
+	return <div className="table-header">Tabelloverskriften kommer her</div>;
 
-				/>
-				<HeaderField
-					name={HeaderFieldName.BRUKER_FNR}
-					text="Fødselsnummer"
-
-				/>
-				<HeaderField
-					name={HeaderFieldName.VEDTAK_STARTET}
-					text="Vedtak startet"
-
-				/>
-				<HeaderField
-					name={HeaderFieldName.STATUS}
-					text="Status"
-
-				/>
-				<HeaderField
-					name={HeaderFieldName.BESLUTTER_NAVN}
-					text="Kvalitetssikrer"
-
-				/>
-				<HeaderField
-					name={HeaderFieldName.VEILEDER_NAVN}
-					text="Veileder"
-
-				/>
-				<HeaderField
-					name={HeaderFieldName.STATUS_ENDRET}
-					text="Status endret"
-
-				/>
-				<HeaderField
-					name={HeaderFieldName.BRUKER_OPPFOLGINGSENHET_NAVN}
-					text="Enhet"
-
-				/>
-			</div>
-		</div>
-	);
 };
 
-interface HeaderFieldProps {
-	name: HeaderFieldName;
-	text: string;
-}
 
-const HeaderField = (props: HeaderFieldProps) => {
-	const { name, text } = props;
-
-
-
-
-	return (
-		<button
-			role="columnheader"
-
-			className="table-header-field"
-		>
-			{text}
-
-		</button>
-	);
-};
